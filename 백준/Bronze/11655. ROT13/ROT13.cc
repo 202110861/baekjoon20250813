1953a10b0e21bf4a0257e46a7c7bf91fe131fdee
202110861 <101165393+202110861@users.noreply.github.com>
2025-08-18 13:54:54 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -11,11 +11,11 @@ string s;
 void ROT13(string str) {
 	for (char c : str) {
 		if (c >= 'a' && c <= 'z') {
-			if (c + 13 > 'z') cout << char((c + 13) - 'z' + 'a'-1);
+			if (c + 13 > 'z') cout << char((c + 13) - 26);
 			else cout << char(c + 13);
 		}
 		else if (c >= 'A' && c <= 'Z') {
-			if (c + 13 > 'Z') cout << char((c + 13) - 'Z' + 'A'-1);
+			if (c + 13 > 'Z') cout << char((c + 13) - 26);
 			else cout << char(c + 13);
 		}
 		else cout << c;
