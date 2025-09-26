edad860a2d73c76d02ae025ff411522cfc450340
202110861 <101165393+202110861@users.noreply.github.com>
2025-09-26 12:49:25 +0900

    [Silver III] Title: 교수가 된 현우, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -18,7 +18,6 @@ int check(long long num) {
 	int count2 = 0, count5 = 0;
 	int two = 2, five = 5;
 	while (num >=two ) {
-		
 		count2 += num / two;
 		two *= 2;
 		
@@ -31,6 +30,22 @@ int check(long long num) {
 	if (count2 == count5) return count2;
 	else if (count2 > count5) return count5;
 	else return count2;
+
+}
+
+//강의 코드
+void check2(long long num) {
+
+	int count2 = 0, count5 = 0;
+	
+	for (int i = 2; i <= num; i *= 2) {
+		count2 += num / i;
+	}
+	for (int i = 5; i <= num; i *= 5) {
+		count5 += num / i;
+	}
+
+	cout << min(count2, count5) << "\n";
 
 }
 
@@ -48,7 +63,8 @@ int main() {
 	for (int i = 0; i < T; i++) {
 		
 
-		cout << check(a[i]) << "\n";
+		//cout << check2(a[i]) << "\n";
+		check2(a[i]);
 
  	}
 	
